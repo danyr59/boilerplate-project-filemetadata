4df5580db59d24dc2c63528c304656b04c9cfe57
danyr59 <danyelrange59@gmail.com>
2022-05-19 17:57:10 -0400

    bug

--- routes/fileanalyse.js
+++ routes/fileanalyse.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const route = express.Router()
 const multer = require("multer")
-const upload = multer({ dest: "uploads/" })
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
 
 
 route.post("/fileanalyse", (req, res, next) => {

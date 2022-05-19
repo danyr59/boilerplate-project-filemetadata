2e89c890870f4241893c7a6bc7b2ed015485736d
danyr59 <danyelrange59@gmail.com>
2022-05-19 14:46:13 -0400

    verificar

--- routes/fileanalyse.js
+++ routes/fileanalyse.js
@@ -4,10 +4,24 @@ const multer = require("multer")
 const upload = multer({ dest: "uploads/" })
 
 
-route.post("/fileanalyse", upload.single("upfile"), (req, res, next) => {
-  console.log(req.body)//object null
-  console.log(req.file) //object contains data upload
-  res.json("hola")
+route.post("/fileanalyse", (req, res, next) => {
+  console.log(req.headers)
+  console.log(req.body)
+  console.log(req.headers['transfer-encoding'])
+  next()
+},/*upload.single("upfile"),*/(req, res, next) => {
+  // console.log(req)//object null
+  // console.log(req.file) //object contains data upload
+  // console.log(req.rawHeaders)
+
+  res.json("esto")
+  // console.log(Object.keys(req))
+  // const { originalname, mimetype, size } = req.file;
+  // res.json({
+  //   name: originalname,
+  //   type: mimetype,
+  //   size
+  // })
 })
 
 module.exports = route;
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var cors = require('cors');
-var bodyParser = require("body-parser")
+// var bodyParser = require("body-parser")
 require('dotenv').config()
 
 var app = express();
 
 //midleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 //routes
 app.use("/api", require("./routes/fileanalyse"));
 app.get('/', function(req, res) {
+  // console.log(req.rawHeaders)
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Your app is listening on port ' + port)
+app.use("port", process.env.PORT || 3000);
+app.listen(app.get("port"), function() {
+  console.log(`Your app is listening on port ${app.get("port")} `)
 });
